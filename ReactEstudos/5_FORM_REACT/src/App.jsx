72dfffc7b2d220bda6cc85b08222ee5bdb60de72
cadumega <72901445+cadumega@users.noreply.github.com>
2022-04-28 03:16:46 -0300

    reactestudos

--- ReactEstudos/5_FORM_REACT/src/App.jsx
+++ ReactEstudos/5_FORM_REACT/src/App.jsx
@@ -7,9 +7,13 @@ function App() {
   return (
     <div className="App">
      <h2>Forms</h2>
-     <MyForm />
+     <MyForm user={{name:"Josias" ,email:"josias@gmmail.com"}} /> 
+     {/* user é um objeto que será passado para MyForm */}
     </div>
   )
 }
 
 export default App
+
+// recebemos a prop user em const MyForm, no proprio input vou poder fazer uma condicional.
+// Assim, consigo uma pré-configuração do formulário.
--- ReactEstudos/5_FORM_REACT/src/components/MyForm.jsx
+++ ReactEstudos/5_FORM_REACT/src/components/MyForm.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './MyForm.css'
 import { useState } from 'react'
 
-const MyForm = () => {
+const MyForm = ({ user }) => {
+    // 6 - Controlled inputs 
     // 3 - gerenciamento de dados
-    let [name, setName] = useState()
-    let [email, setEmail] = useState()
+    let [name, setName] = useState(user ? user.name : "")
+    let [email, setEmail] = useState(user ? user.email : "")
 
     const handleName = (e) => {
         // console.log("Mudou o nome");
@@ -13,21 +14,29 @@ const MyForm = () => {
     }
     // Mudar o estado a cada vez que o name for alterado, + fácil a manutenção
 
+    const handleSubmit = (e) => {
+        e.preventDefault();                // Ira parar o evento padrão do formulário
+        console.log(name, email);
+    }
+
     // console.log(name);
     console.log(email);
 
     return (
         <div>
-            <form>
+            {/* envio de form */}
+            <form onSubmit={handleSubmit}>  {/* onSubmit é um evento que é disparado quando o formulário é enviado */}
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder='Digite o seu nome' onChange={handleName} />
+                    <input type="text" name="name" value={name} placeholder='Digite o seu nome' onChange={handleName} />
                 </div>
                 {/* lable envolvendo input */}
+
+                {/* simplificacao de manipulacao de state */}
                 <label>
                     <span>E-mail </span>
-                    <input type="email" name="email" placeholder='Digite o seu e-mail' 
-                    onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" name="email" value={email} placeholder='Digite o seu e-mail'
+                        onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
